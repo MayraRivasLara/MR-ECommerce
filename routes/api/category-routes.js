0c7ba2bdd2a55fd72eab02b229f2019db568e661
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-03-14 01:48:03 +0800

    Create new category endpoint done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,7 +28,13 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  }).then((newCategory) => {
+    res.json(newCategory);
+  })
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
