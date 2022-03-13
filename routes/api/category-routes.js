4ce1614f0ef40b3bded8809f2fd014c0d7280d9c
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-03-14 02:00:36 +0800

    update endpoint to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,6 +38,15 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  }, {
+    where: {
+      id:req.params.id
+    }
+  }).then((updated) => {
+    res.json(updated);
+  })
 });
 
 router.delete('/:id', (req, res) => {
