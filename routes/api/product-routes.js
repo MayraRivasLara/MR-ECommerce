07556fc2b552f530e21e0eddfe0179ee08c9ac0e
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-03-14 13:46:00 +0800

    findall end point working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+
+  Product.findAll({
+    // be sure to include its associated Category and Tag data
+    include: [
+      {model: Category}
+    ]
+  }).then((products) => {
+    res.json(products);
+  })
 });
 
 // get one product
